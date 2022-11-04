3502792371032d94383fdeea18e27d774d87642d
Daniel Cybis <115581800+daniel-cybis@users.noreply.github.com>
2022-11-04 17:41:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ function createGame(player1, hour, player2) {
             </li>
   `
 }
- 
+ let delay = 0.2;
 function creatCard(date, day, games) {
+  delay = delay + 0.2;
   return `
-   <div class="card">
+   <div class="card" style="animation-delay:${delay}s">
           <h2>${date}<span>${day}</span></h2>
           <ul>
            ${games}
@@ -21,8 +22,5 @@ function creatCard(date, day, games) {
 
 document.querySelector("#cards").innerHTML =
   creatCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
-
   creatCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
-  
   creatCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
-  
